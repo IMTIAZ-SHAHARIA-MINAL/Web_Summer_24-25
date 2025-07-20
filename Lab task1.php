853a7aeba662cdd41c546e7c1035bfef5ae3667d
IMTIAZ-SHAHARIA-MINAL <103626288+IMTIAZ-SHAHARIA-MINAL@users.noreply.github.com>
2025-07-20 10:43:33 +0600

    Add files via upload

---
+++ Lab task1.php
@@ -0,0 +1,37 @@
+<DOCTYPE htmal>
+<html>
+
+<title> 
+</title>
+
+<body>
+    <center>
+        <h1 style="color:blue;"> AIUB </h1>
+        <h3 style="color:blue;"> Registration form </h3>
+
+</center>
+<b> Complete the Registration </b><br>
+<table>
+            <td> Full Name: </td><td><input type "text"><td><tr>
+            <td>Email: </td><td><input type "text"><td><tr>
+            <td>Password: </td><td><input type "text"><td><tr>
+
+<td>Gender:
+</td><td><input type="radio" name = "des">Male<td>
+<input type="radio" name= "des">Other<td></tr>
+<tr><td> Langulage Known:</td><td><input type="checkbox"> English
+                                  <input type="checkbox"> Bangla
+                                  <input type="checkbox"> Hindi
+<tr><td> Country: </td><td><select name="Country">
+                           <option value="1">--Select--</option>
+                           <option value="2">Bangladesh</option>
+                           <option value="2">India</option></select></td>
+<tr><td> Date of birth: </td><td><input type="date"><td></tr>
+<tr><td> Upload Photo: </td><td><input type="file"><td></tr>
+<tr><td> Comments: </td><td><input type="textbox"><td></tr>
+
+<tr><td><input type="button" name="submit"><td></tr>
+
+
+</body>
+</html>
